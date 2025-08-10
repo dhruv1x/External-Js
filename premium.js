429c0239eea186067c65bb2f1c932031b95f2536
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2025-08-10 19:50:48 +0530

    Update premium.js

--- premium.js
+++ premium.js
@@ -1,8 +1,8 @@
 // streams.js
 const streamMap = {
-  "sky": {
-    url: "https://1rinij81ehes2uswech6drot7durixl2r0.rtgjmfvikmgtgrvkpbg.shop/j5ru6icHofROsespidec64w0s7lgo8ot.m3u8",
-    poster: "https://i.ibb.co/5h7wDqnk/IMG-20250709-105025.jpg"
+  "IOS": {
+    url: "https://d17lsiabqrlwa2.cloudfront.net/pl_138/207480-6542820-1/playlist.m3u8",
+    poster: "#"
   },
   "tencric": {
     url: "https://edge3-moblive.yuppcdn.net/drm/smil:tencricketdrm.smil/master.m3u8",
