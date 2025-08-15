99ee81b70b101e1450564fe09bfe414957c7bfbb
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2025-08-16 00:57:43 +0530

    Update ifm.js

--- ifm.js
+++ ifm.js
@@ -12,7 +12,7 @@ if (confirm("Join Our Telegram Channel @cricxfootball")) {
 // ✅ Streams list
 const streams = {
     m1: "https://la14hd.com/vivo/canal.php?stream=usanetwork",
-    m2: "https://golazohd.com/online/canal.php?stream=tycsports",
+    m2: "https://aboutgame.cx/idea/?id=1",
     m3: "#",
     m4: "https://streamtp4.com/global1.php?stream=disney10",
     m5: "https://streamtp4.com/global1.php?stream=disney12",
