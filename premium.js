1aac2c26a67738d4fa8edd12279252ba5dec093a
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2025-08-16 18:57:13 +0530

    Update premium.js

--- premium.js
+++ premium.js
@@ -1,15 +1,15 @@
 // streams.js
 const streamMap = {
-  "IOS": {
-    url: "https://d17lsiabqrlwa2.cloudfront.net/pl_138/207480-6542820-1/playlist.m3u8",
+  "PL3": {
+    url: "https://d17lsiabqrlwa2.cloudfront.net/pl_138/207480-6552990-1/chunklist.m3u8",
     poster: "#"
   },
   "tencric": {
     url: "https://edge3-moblive.yuppcdn.net/drm/smil:tencricketdrm.smil/master.m3u8",
     poster: "#"
   },
-  "CLUB": {
-    url: "https://rmtv.akamaized.net/hls/live/2043153/rmtv-es-web/master.m3u8",
+  "PL2": {
+    url: "https://d17lsiabqrlwa2.cloudfront.net/pl_138/207480-6552988-1/chunklist.m3u8",
     poster: "#"
   },
   "willow": {
@@ -18,6 +18,10 @@ const streamMap = {
   },
   "PL1": {
     url: "https://d17lsiabqrlwa2.cloudfront.net/pl_138/207480-6552888-1/chunklist.m3u8",
+    poster: ""
+  },
+  "PL4": {
+    url: "https://d17lsiabqrlwa2.cloudfront.net/pl_138/207480-6552996-1/chunklist.m3u8",
     poster: ""
   },
 
