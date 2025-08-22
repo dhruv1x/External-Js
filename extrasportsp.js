29fa49f5af83265c71752e485ffa4ef496ba449d
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2025-08-23 00:07:10 +0530

    Create extrasportsp.js

---
+++ extrasportsp.js
@@ -0,0 +1,26 @@
+const streamMap = {
+  "B1": {
+    url: "https://d17lsiabqrlwa2.cloudfront.net/pl_138/207480-6562072-1/chuncklist.m3u8",
+    poster: "#",
+    telegramHandle: "@cricxfootball",
+    popupMessage: "Join Our Telegram Channel @cricxfootball"
+  },
+  "LG1": {
+    url: "https://d17lsiabqrlwa2.cloudfront.net/pl_138/207480-6562090-1/chunklist.m3u8",
+    poster: "#",
+    telegramHandle: "@cricxfootball",
+    popupMessage: "Join Our Telegram Channel @cricxfootball"
+  },
+  "fox501": {
+    url: "#",
+    poster: "https://i.ibb.co/dNp10Xf/IMG-20250620-145214.jpg",
+    telegramHandle: "@FoxCricketHQ",
+    popupMessage: "Live on Fox Cricket! Join @FoxCricketHQ on Telegram."
+  },
+  "willow": {
+    url: "https://example.com/willow.m3u8",
+    poster: "https://i.ibb.co/V0ZxtGjH/IMG-20250706-160925.jpg",
+    telegramHandle: "@WillowCricketTV",
+    popupMessage: "Watch Willow TV Live – Join our Telegram @WillowCricketTV!"
+  }
+};
