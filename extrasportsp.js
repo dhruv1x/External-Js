138933cf9f7c73361fec7a071016163a7ae1270b
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2025-08-23 00:59:10 +0530

    Update extrasportsp.js

--- extrasportsp.js
+++ extrasportsp.js
@@ -18,7 +18,7 @@ const streamMap = {
     popupMessage: "Join Our Telegram Channel @cricxfootball"
   },
   "W1": {
-    url: "http://d17lsiabqrlwa2.cloudfront.net/pl_138/207480-6536194-1/chuncklist.m3u8",
+    url: "https://d17lsiabqrlwa2.cloudfront.net/pl_138/207480-6536194-1/chuncklist.m3u8",
     poster: "#",
     telegramHandle: "@cricxfootball",
     popupMessage: "Join Our Telegram Channel @cricxfootball"
