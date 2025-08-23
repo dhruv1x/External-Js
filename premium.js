c1d902af6963dbb7c3c1c5a9e86745b5daf934e0
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2025-08-23 21:35:25 +0530

    Update premium.js

--- premium.js
+++ premium.js
@@ -8,8 +8,8 @@ const streamMap = {
     url: "https://edge3-moblive.yuppcdn.net/drm/smil:tencricketdrm.smil/master.m3u8",
     poster: "#"
   },
-  "SCF": {
-    url: "https://d17lsiabqrlwa2.cloudfront.net/pl_118/207480-6563650-1/playlist.m3u8",
+  "LIG1": {
+    url: "https://d17lsiabqrlwa2.cloudfront.net/pl_138/207480-6562090-1/chunklist.m3u8",
     poster: "#"
   },
   "willow": {
