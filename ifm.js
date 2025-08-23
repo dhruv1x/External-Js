c82ff6ae1f36cdcc55461a422322ae8ca4dfa3dc
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2025-08-23 19:55:30 +0530

    Update ifm.js

--- ifm.js
+++ ifm.js
@@ -12,7 +12,7 @@ if (confirm("Join Our Telegram Channel @cricxfootball")) {
 // ✅ Streams list
 const streams = {
     m1: "https://aboutgame.cx/idea/?id=1",
-    m2: "https://aboutgame.cx/idea/?id=4",
+    m2: "https://aboutgame.cx/idea/?id=2",
     m3: "https://aboutgame.cx/idea/?id=3",
     m4: "https://aboutgame.cx/idea/?id=ufc",
     m5: "https://streamtp4.com/global1.php?stream=disney12",
