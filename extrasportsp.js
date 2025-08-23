b413d7c3bfedcfb6196bbac23bf40ef7126ecbf9
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2025-08-23 16:56:13 +0530

    Update extrasportsp.js

--- extrasportsp.js
+++ extrasportsp.js
@@ -1,18 +1,18 @@
 const streamMap = {
   "B1": {
-    url: "https://d17lsiabqrlwa2.cloudfront.net/pl_138/207480-6562072-1/chuncklist.m3u8",
+    url: "#",
     poster: "#",
     telegramHandle: "@cricxfootball",
     popupMessage: "Join Our Telegram Channel @cricxfootball"
   },
   "LG1": {
-    url: "https://d17lsiabqrlwa2.cloudfront.net/pl_138/207480-6562090-1/chunklist.m3u8",
+    url: "#",
     poster: "#",
     telegramHandle: "@cricxfootball",
     popupMessage: "Join Our Telegram Channel @cricxfootball"
   },
   "PL1": {
-    url: "https://d17lsiabqrlwa2.cloudfront.net/pl_138/207480-6555644-1/chunklist.m3u8",
+    url: "#",
     poster: "#",
     telegramHandle: "@cricxfootball",
     popupMessage: "Join Our Telegram Channel @cricxfootball"
@@ -24,7 +24,7 @@ const streamMap = {
     popupMessage: "Join Our Telegram Channel @cricxfootball"
   },
   "L1": {
-    url: "https://d17lsiabqrlwa2.cloudfront.net/pl_138/207480-6551168-1/chuncklist.m3u8",
+    url: "#",
     poster: "#",
     telegramHandle: "@cricxfootball",
     popupMessage: "Join Our Telegram Channel @cricxfootball"
