b7a014d78f46b80aef97062c9739bc943f306dac
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2025-08-30 17:10:16 +0530

    Create adds.js

---
+++ adds.js
@@ -0,0 +1,15 @@
+const streams = {
+  "PL1": "https://givemereddit.link/soccer/chelsea.html",
+  "PL2": "https://goalovo.net/events/stream1/",
+  "CC3": "https://sportey.tv/multistream/16/Sky%20Sports%20Football?v=channelFrame_1756320643401_u950dgyxcqrfwp6gp",
+  "UCL1": "https://goalovo.net/events/stream3/",
+  "DFB1": "https://goalovo.net/events/stream2/",
+  "VC1": "https://voodc.com/embed/1/85818b97a087a186847a8597a48c9885867f.html",
+  "SC1": "https://sportzonline.site/channels/bra/br5.php",
+  "SC2": "https://streambtw.live/iframe/ch3.php",
+  "M1": "https://aboutgame.cx/adfree/?id=1",
+  "M2": "https://aboutgame.cx/adfree/?id=2",
+  "M3": "https://aboutgame.cx/adfree/?id=3",
+  "M4": "https://aboutgame.cx/adfree/?id=4"
+  
+};
