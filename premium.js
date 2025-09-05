2acd016a13838197e1d9544b3860135b23ff0237
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2025-09-05 05:47:18 +0530

    Update premium.js

--- premium.js
+++ premium.js
@@ -8,8 +8,8 @@ const streamMap = {
     url: "https://edge3-moblive.yuppcdn.net/drm/smil:tencricketdrm.smil/master.m3u8",
     poster: "#"
   },
-  "B1": {
-    url: "https://d17lsiabqrlwa2.cloudfront.net/pl_138/207480-6562072-1/chuncklist.m3u8",
+  "L1": {
+    url: "https://jireh-1-hls-video-ar-isp.dps.live/hls-video/567ffde3fa319fadf3419efda25619456231dfea/latina/latina.smil/playlist.m3u8?dpssid=b131769307717666dd4b364958&sid=ba5t1l1xb21925953729666dd4b4dc792&ndvc=0",
     poster: "#"
   },
   "willow": {
@@ -26,6 +26,10 @@ const streamMap = {
   },
     "S2": {
     url: "https://dtvxtream.com/sonyliv2/300273.m3u8",
+    poster: ""
+  },
+  "L2": {
+    url: "https://live-evg20.tv360.bitel.com.pe/bitel/latina_abr/playlist.m3u8",
     poster: ""
   },
 
