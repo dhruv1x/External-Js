2f26a93a68334fbfbea3a8d20c63244fcffe58b9
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2025-09-06 01:01:23 +0530

    Update ifm.js

--- ifm.js
+++ ifm.js
@@ -11,7 +11,7 @@ if (confirm("Join Our Telegram Channel @cricxfootball")) {
 
 // ✅ Streams list
 const streams = {
-    m1: "https://streamysport.org/part/Ll.php?id=3/56",
+    WWE: "https://veplay.top/stream/2f2d7e70-54a7-4b7f-b059-cb6fd3a1a2cc",
     m2: "https://aboutgame.cx/idea/?id=1&utm_source=chatgpt.com",
     m3: "https://aboutgame.cx/idea/?id=3",
     m4: "https://aboutgame.cx/idea/?id=ufc",
