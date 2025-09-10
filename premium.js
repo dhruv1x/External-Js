dd9151c99d3a394cb93588475f4690d9235f4161
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2025-09-10 20:54:26 +0530

    Update premium.js

--- premium.js
+++ premium.js
@@ -1,7 +1,7 @@
 // streams.js
 const streamMap = {
   "Fox": {
-    url: "https://hugh.cdn.rumble.cloud/live/t34ch9px/slot-7/65wi-d07h/chunklist.m3u8",
+    url: "https://hugh.cdn.rumble.cloud/live/gi29le7p/slot-103/168w-yexl/chunklist.m3u8",
     poster: "#"
   },
   "tencric": {
