b17d1929ec624349062d9b7e0c7d03aa7fd5da53
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2025-09-26 01:16:01 +0530

    Update adds.js

--- adds.js
+++ adds.js
@@ -1,7 +1,7 @@
 const streams = {
   "A1": "https://iframe-f0p.pages.dev/English",
   "PB": "https://embedsports.top/embed/echo/pakistan-sri-lanka-t20-asia-cup-cricket2203/1",
-  "L1": "https://streamkick.org/games/stream1/",
+  "L1": "https://voodc.com/embed/85818b99a489a18a847a85989f8d9885877e.html",
   "BD": "https://vivozly.com/canal.php?stream=event1",
   "PL2": "https://goalovo.net/events/stream2/",
   "VC1": "https://voodc.com/embed/1/85818b97a087a186847a8597a48c9885867f.html",
