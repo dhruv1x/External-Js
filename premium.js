e99ca071885e60f8b4f560299ca895ab81820d6a
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2025-09-26 19:54:59 +0530

    Update premium.js

--- premium.js
+++ premium.js
@@ -1,7 +1,7 @@
 // streams.js
 const streamMap = {
   "Fox": {
-    url: "https://hugh.cdn.rumble.cloud/live/v0xi25uh/slot-85/xop9-bbmz/chunklist_DVR.m3u8",
+    url: "https://hugh.cdn.rumble.cloud/live/4gf18j5s/slot-9/h01l-92sj/chunklist_DVR.m3u8",
     poster: "#"
   },
   "willow": {
