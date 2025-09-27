6a0d2f49fc91420cf784efc3f191a549e749ae7d
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2025-09-27 19:50:34 +0530

    Update adds.js

--- adds.js
+++ adds.js
@@ -6,7 +6,7 @@ const streams = {
   "PL2": "https://goalovo.net/events/stream2/",
   "VC1": "https://voodc.com/embed/1/85818b97a087a186847a8597a48c9885867f.html",
   "SC1": "https://sportzonline.site/channels/bra/br5.php",
-  "L2": "https://sportstvnow.org/games/stream5/",
+  "L1": "https://streamkick.org/games/stream5/",
   "M1": "https://aboutgame.cx/adfree/?id=1",
   "M2": "https://aboutgame.cx/adfree/?id=2",
   "M3": "https://aboutgame.cx/adfree/?id=3",
