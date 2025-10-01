71a2117cf35ff5ceda16ae211527ae9b4ee25d85
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2025-10-02 00:03:00 +0530

    Update adds.js

--- adds.js
+++ adds.js
@@ -6,9 +6,9 @@ const streams = {
   "PL2": "https://goalovo.net/events/stream2/",
   "VC1": "https://voodc.com/embed/1/85818b97a087a186847a8597a48c9885867f.html",
   "U2": "https://streamkick.org/games/stream2/",
-  "U1": "https://streamkick.org/games/stream1/",
-  "U3": "",
-  "M2": "https://aboutgame.cx/adfree/?id=2",
+  "U1": "https://streamkick.org/games/stream3/",
+  "U3": "https://streamkick.org/games/stream1/",
+  "U4": "https://streamkick.org/games/stream5/",
   "M3": "https://aboutgame.cx/adfree/?id=3",
   "M4": "https://aboutgame.cx/adfree/?id=4"
   
