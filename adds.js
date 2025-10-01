42786d371407af095b2300ce14086db09ba99890
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2025-10-01 22:09:50 +0530

    Update adds.js

--- adds.js
+++ adds.js
@@ -5,9 +5,9 @@ const streams = {
   "BD": "https://vivozly.com/canal.php?stream=event1",
   "PL2": "https://goalovo.net/events/stream2/",
   "VC1": "https://voodc.com/embed/1/85818b97a087a186847a8597a48c9885867f.html",
-  "U2": "https://streamkick.org/games/stream4/",
-  "U1": "https://streamkick.org/games/stream2/",
-  "U3": "https://streamkick.org/games/stream1/",
+  "U2": "https://streamkick.org/games/stream2/",
+  "U1": "https://streamkick.org/games/stream1/",
+  "U3": "",
   "M2": "https://aboutgame.cx/adfree/?id=2",
   "M3": "https://aboutgame.cx/adfree/?id=3",
   "M4": "https://aboutgame.cx/adfree/?id=4"
