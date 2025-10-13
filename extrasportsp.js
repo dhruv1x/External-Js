5c70f07f504c49a7c6b125431a61cc6b3bac2772
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2025-10-13 09:56:46 +0530

    Update extrasportsp.js

--- extrasportsp.js
+++ extrasportsp.js
@@ -18,7 +18,7 @@ const streamMap = {
     popupMessage: "Join Our Telegram Channel @cricxfootball"
   },
   "A1": {
-    url: "https://lchdvideo.live/sky1nz.m3u8",
+    url: "https://2e47f5d7638848ff8a74309f661ae9e1.mediatailor.us-east-1.amazonaws.com/v1/manifest/85b2e189604a6043ef957e7a3e6ed3bf9b11c843/USCA_DAI_STRM6/a647c92c-60b4-4c9f-8904-fcfeb50bcab6/1.m3u8",
     poster: "#",
     telegramHandle: "@livecrichdofficial",
     popupMessage: "Join Our Telegram Channel @livecrichdofficial"
