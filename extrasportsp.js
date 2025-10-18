3850ec4f012841890749cfe4f0faae6c57bf3798
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2025-10-18 19:30:52 +0530

    Update extrasportsp.js

--- extrasportsp.js
+++ extrasportsp.js
@@ -17,11 +17,11 @@ const streamMap = {
     telegramHandle: "@cricxfootball",
     popupMessage: "Join Our Telegram Channel @cricxfootball"
   },
-  "A1": {
-    url: "https://2e47f5d7638848ff8a74309f661ae9e1.mediatailor.us-east-1.amazonaws.com/v1/manifest/85b2e189604a6043ef957e7a3e6ed3bf9b11c843/USCA_DAI_STRM6/a647c92c-60b4-4c9f-8904-fcfeb50bcab6/1.m3u8",
+  "PCK": {
+    url: "https://lchdvideo.live/cry-v-afc.m3u8",
     poster: "#",
-    telegramHandle: "@livecrichdofficial",
-    popupMessage: "Join Our Telegram Channel @livecrichdofficial"
+    telegramHandle: "@cricxfootball",
+    popupMessage: "Join Our Telegram Channel @cricxfootball"
   },
   "PL4": {
     url: "https://lchdvideo.live/sky5nz.m3u8",
