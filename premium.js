fa916c907c40a352bae74750980329ae43c42631
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2025-10-18 21:56:06 +0530

    Update premium.js

--- premium.js
+++ premium.js
@@ -4,16 +4,16 @@ const streamMap = {
     url: "https://v5on.site/PROXIMITY/cric.php?live=https://prod-fastly-us-west-2.video.pscp.tv/Transcoding/v1/hls/aq1zFswizB2Gyk9OFoZXrBWTJnXNKIhtEhEWwienmQAn-ieUtHAUjtBmK00EV-PVRo85sx7_D4niRN-_35C4Zg/transcode/us-west-2/periscope-replay-direct-prod-us-west-2-private/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsInZlcnNpb24iOiIyIn0.eyJFbmNvZGVyU2V0dGluZyI6ImVuY29kZXJfc2V0dGluZ18xMDgwcDMwXzEwIiwiSGVpZ2h0IjoxMDgwLCJLYnBzIjo1NTAwLCJXaWR0aCI6MTkyMH0.OImMZabKYJ0cs9CnIapU-4aBk6KNBiJxi1hh-6l4BZ4/st_dynamic_delta.m3u8?type=live",
     poster: "#"
   },
-  "IFA": {
-    url: "https://ssen-live.multitvsolution.com/match_7/EBFCvsMBSG/HLS/master.m3u8",
+  "B1": {
+    url: "https://pull.niues.live/live/stream-380460_lsd.m3u8?auth_key=1760807722-0-0-e5d20e15ef8f82f13ec89d757a9dcac7",
     poster: "#"
   },
   "S1": {
     url: "https://lchdvideo.live/willowcricket.m3u8",
     poster: "#"
   },
-  "S2": {
-    url: "https://pull.niues.live/live/stream-536185_lsd.m3u8?auth_key=1760731342-0-0-24e633ffb87a0bb1eca2c15eb5da7e76",
+  "PL1": {
+    url: "https://pull.niur.live/live/stream-9912060_lsd.m3u8?txSecret=15cb84d02a035aec57631cf80c3409c6&txTime=68f4126a",
     poster: ""
   },
   "UFC": {
