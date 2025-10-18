9139fd0304ef950f8e5742c51d8eb0c19459c836
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2025-10-18 21:48:55 +0530

    Update extrasportsp.js

--- extrasportsp.js
+++ extrasportsp.js
@@ -1,36 +1,36 @@
 const streamMap = {
-  "PL1": {
-    url: "https://lchdvideo.live/skyselectnz.m3u8",
+  "PL": {
+    url: "#",
     poster: "",
     telegramHandle: "@cricxfootball",
     popupMessage: "Join Our Telegram Channel @cricxfootball"
   },
   "PL2": {
-    url: "https://lchdvideo.live/sky3nz.m3u8",
+    url: "#",
     poster: "#",
     telegramHandle: "@cricxfootball",
     popupMessage: "Join Our Telegram Channel @cricxfootball"
   },
   "PL3": {
-    url: "https://lchdvideo.live/sky4nz.m3u8",
+    url: "#",
     poster: "#",
     telegramHandle: "@cricxfootball",
     popupMessage: "Join Our Telegram Channel @cricxfootball"
   },
   "PCK": {
-    url: "https://lchdvideo.live/cry-v-afc.m3u8",
+    url: "#",
     poster: "#",
     telegramHandle: "@cricxfootball",
     popupMessage: "Join Our Telegram Channel @cricxfootball"
   },
   "PL4": {
-    url: "https://lchdvideo.live/sky5nz.m3u8",
+    url: "#",
     poster: "#",
     telegramHandle: "@cricxfootball",
     popupMessage: "Join Our Telegram Channel @cricxfootball"
   },
-  "PL5": {
-    url: "https://lchdvideo.live/sky7nz.m3u8",
+  "PL1": {
+    url: "https://lchdvideo.live/sky8nz.m3u8",
     poster: "#",
     telegramHandle: "@cricxfootball",
     popupMessage: "Join Our Telegram Channel @cricxfootball"
