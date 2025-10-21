c50f26835eff5627e705b60340e90b7c5ec0dba2
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2025-10-21 22:10:14 +0530

    Update adds.js

--- adds.js
+++ adds.js
@@ -1,5 +1,5 @@
 const streams = {
-  "PFL": "https://embednow.top/embed/pfl-champions-series-dubai",
+  "T1": "https://streamkick.org/home/blog1/",
   "PB": "https://embedsports.top/embed/echo/pakistan-sri-lanka-t20-asia-cup-cricket2203/1",
   "1": "https://voodc.com/embed/85818b99a489a18a847a85989f8d9885877e.html",
   "BD": "https://vivozly.com/canal.php?stream=event1",
