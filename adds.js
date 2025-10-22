7f982d8af7b1810747d8e0df7dc2606b10d98984
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2025-10-22 22:16:13 +0530

    Update adds.js

--- adds.js
+++ adds.js
@@ -1,7 +1,7 @@
 const streams = {
-  "T1": "https://streamkick.org/home/blog2/",
-  "T2": "https://streamkick.org/home/blog4/",
-  "T3": "https://streamkick.org/home/blog1/",
+  "T2": "https://streamkick.org/home/blog2/",
+  "T3": "https://streamkick.org/home/blog4/",
+  "T1": "https://streamkick.org/home/blog1/",
   "T4": "https://streamkick.org/home/blog3/",
   "T5": "https://streamkick.org/home/blog5/",
   "T6": "https://streamkick.org/home/blog6/",
