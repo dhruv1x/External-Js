363450a50799393cfe2b8c57181732dae5a87deb
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2025-10-26 00:05:26 +0530

    Update adds.js

--- adds.js
+++ adds.js
@@ -8,8 +8,8 @@ const streams = {
   "T7": "https://matchio.zip/home/blog6/",
   "U1": "https://voodc.com/embed/1/85818c929d8ca28a847a85989f8a9885887f.html",
   "PL": "https://streamkick.org/games/stream1/",
-  "U4": "https://streamkick.org/games/stream5/",
-  "M3": "https://aboutgame.cx/adfree/?id=3",
+  "F1": "https://matchio.zip/home/blogf/",
+  "UFC": "https://matchio.zip/mma/blog/",
   "S1": "https://matchio.zip/home/blog4/"
   
 };
