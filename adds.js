efcf6a80ef68ae4f1024b492e5b714b52d1428f0
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2025-10-25 21:27:22 +0530

    Update adds.js

--- adds.js
+++ adds.js
@@ -10,6 +10,6 @@ const streams = {
   "PL1": "https://streamkick.org/games/stream1/",
   "U4": "https://streamkick.org/games/stream5/",
   "M3": "https://aboutgame.cx/adfree/?id=3",
-  "F1": "https://streamkick.org/f1streams/streams/"
+  "S1": "https://matchio.zip/home/blog4/"
   
 };
