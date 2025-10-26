9a1ea494de95c1af86016da203eeed24921e4aab
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2025-10-26 20:23:40 +0530

    Update premium.js

--- premium.js
+++ premium.js
@@ -4,8 +4,8 @@ const streamMap = {
     url: "https://pull.niur.live/live/stream-462413_lhd.m3u8?txSecret=d994b674626dfd32ed612f9ca8470e6d&txTime=68fd6a37",
     poster: "#"
   },
-  "LG1": {
-    url: "https://pull.niues.live/live/stream-285067_lsd.m3u8?auth_key=1761422783-0-0-344febcb5d056050ad3538a54e87bec9",
+  "L1": {
+    url: "https://rumble.com/live-hls-dvr/6ymiwo/playlist.m3u8",
     poster: "#"
   },
   "Fox2": {
