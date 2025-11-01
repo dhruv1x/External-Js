250d7eb1798ffff0395022038c52517050e51d0d
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2025-11-01 22:51:03 +0530

    Update adds.js

--- adds.js
+++ adds.js
@@ -5,7 +5,7 @@ const streams = {
   "PL4": "https://matchio.zip/home/blog4/",
   "PL5": "https://matchio.zip/home/blog5/",
   "L1": "https://matchio.zip/home/blog6/",
-  "T7": "https://matchio.zip/home/blog6/",
+  "B1": "https://matchio.zip/home/blog3/",
   "U1": "https://voodc.com/embed/1/85818c929d8ca28a847a85989f8a9885887f.html",
   "PL": "https://matchio.zip/home/blog1/",
   "F1": "https://matchio.zip/home/blogf/",
