cdd323fc730a3ee79a24d26e652323a44b00a8f3
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2025-11-05 01:00:00 +0530

    Update adds.js

--- adds.js
+++ adds.js
@@ -4,12 +4,12 @@ const streams = {
   "PL3": "https://matchio.zip/home/blog3/",
   "PL4": "https://matchio.zip/home/blog4/",
   "PL5": "https://matchio.zip/home/blog5/",
-  "L1": "https://matchio.zip/home/blog2/",
-  "B1": "https://matchio.zip/home/blog3/",
-  "1": "https://voodc.com/embed/1/85818c929d8ca28a847a85989f8a9885887f.html",
-  "LG1": "https://matchio.zip/home/blog7/",
+  "U3": "https://matchio.zip/home/blog4/",
+  "U4": "https://matchio.zip/home/blog3/",
+  "U5": "https://matchio.zip/home/blog5/",
+  "U6": "https://matchio.zip/home/blog6/",
   "F1": "https://matchio.zip/home/blogf/",
   "UFC": "https://matchio.zip/mma/blog/",
-  "S1": "https://matchio.zip/home/blog2/"
+  "U7": "https://matchio.zip/home/blog7/"
   
 };
