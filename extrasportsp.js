9e22d952944e8acd4d6b054398f3de84e9a2f4a6
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2025-11-10 01:38:34 +0530

    Update extrasportsp.js

--- extrasportsp.js
+++ extrasportsp.js
@@ -1,6 +1,6 @@
 const streamMap = {
   "L1": {
-    url: "https://in-mc-fdlive.fancode.com/mumbai/130944_english_hls_63ee91f7cb12045_1adfreeta-di_h264/index.m3u8",
+    url: "https://in-mc-fdlive.fancode.com/mumbai/130946_english_hls_fc669aa86861806_1adfreeta-di_h264/index.m3u8",
     poster: "",
     telegramHandle: "@cricxfootball",
     popupMessage: "Join Our Telegram Channel @cricxfootball"
