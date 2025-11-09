9d4dbfbb8d1d34523ba7552cb78fe62d5deaa7ae
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2025-11-09 20:20:06 +0530

    Update adds.js

--- adds.js
+++ adds.js
@@ -3,7 +3,7 @@ const streams = {
   "PL2": "https://matchio.zip/home/blog3/",
   "PL3": "https://matchio.zip/home/blog2/",
   "PL4": "https://matchio.zip/home/blog4/",
-  "B1": "https://matchio.zip/home/blog3/",
+  "L1": "https://matchio.zip/home/blog5/",
   "U3": "https://matchio.zip/home/blog3/",
   "U4": "https://matchio.zip/home/blog5/",
   "U5": "https://matchio.zip/home/blog4/",
