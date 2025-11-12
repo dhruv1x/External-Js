769263c3f07aa4b2246e4ca6023d9dbcd9f18031
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2025-11-12 14:00:19 +0530

    Update extrasportsp.js

--- extrasportsp.js
+++ extrasportsp.js
@@ -5,13 +5,13 @@ const streamMap = {
     telegramHandle: "@cricxfootball",
     popupMessage: "Join Our Telegram Channel @cricxfootball"
   },
-  "S2": {
+  "S1": {
     url: "https://amg01269-amg01269c1-sportstribal-emea-5204.playouts.now.amagi.tv/playlist/amg01269-willowtvfast-willowplus-sportstribalemea/playlist.m3u8",
     poster: "#",
     telegramHandle: "@livecrichdofficial",
     popupMessage: "Join Our Telegram Channel @livecrichdofficial"
   },
-  "S1": {
+  "S2": {
     url: "https://flussonic.superfasthdserver.com/TenSports/index.m3u8",
     poster: "#",
     telegramHandle: "@livecrichdofficial",
