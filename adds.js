74c35bd4961bb735c004c7ef0652977942908851
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2025-11-23 23:54:21 +0530

    Update adds.js

--- adds.js
+++ adds.js
@@ -3,13 +3,13 @@ const streams = {
   "PL2": "https://matchio.zip/home/blog3/",
   "PL3": "https://matchio.zip/home/blog2/",
   "PL4": "https://matchio.zip/home/blog4/",
-  "L1": "https://matchio.zip/home/blog5/",
+  "L": "https://matchio.zip/home/blog5/",
   "U3": "https://matchio.zip/home/blog3/",
   "U4": "https://matchio.zip/home/blog5/",
   "U5": "https://matchio.zip/home/blog4/",
-  "U6": "https://matchio.zip/home/blog6/",
+  "L1": "https://ballcontrol.click/home/blog2/",
   "F1": "https://ballcontrol.click/home/blogf/",
   "UFC": "https://ballcontrol.click/mma/blog/",
-  "U7": "https://matchio.zip/home/blog7/"
+  "S1": "https://ballcontrol.click/home/blog1/"
   
 };
