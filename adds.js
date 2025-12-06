0c2a90d346dfdae01f030dbc944281096072f7da
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2025-12-06 20:13:49 +0530

    Update adds.js

--- adds.js
+++ adds.js
@@ -4,12 +4,12 @@ const streams = {
   "PL3": "https://ballcontrol.click/home/blog4/",
   "PL4": "https://ballcontrol.click/home/blog3/",
   "PL5": "https://ballcontrol.click/home/blog5/",
-  "U5": "https://ballcontrol.click/home/blog5/",
+  "B1": "https://ballcontrol.click/home/blog8/",
   "L2": "https://voodc.com/embed/85818c95a38f9d8c847a8597a3909885857d.html",
-  "LG1": "https://ballcontrol.click/home/blog6/",
-  "L1": "https://ballcontrol.click/home/blog7",
+  "LG1": "https://ballcontrol.click/home/blog2/",
+  "L1": "https://ballcontrol.click/home/blog6/",
   "F1": "https://ballcontrol.click/home/blogf/",
   "UFC": "https://ballcontrol.click/mma/blog/",
-  "S1": "https://ballcontrol.click/home/blog4/"
+  "S1": "https://ballcontrol.click/home/blog7/"
   
 };
