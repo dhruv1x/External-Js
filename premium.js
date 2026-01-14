4f547e5c0ef1c37548c6eb2671fdf3e412b1c9c4
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2026-01-14 13:21:20 +0530

    Update premium.js

--- premium.js
+++ premium.js
@@ -1,14 +1,14 @@
 // streams.js
 const streamMap = {
-  "S": {
-    url: "56=UNSIGNED-PAYLOAD&X-Amz%2F20251216%2Feu-central-2%2Fs3%2Faws4_request&X-Amz-Date=20251216T085345Z&X-Amz-Expires=3600&X-Amz-Signature=f9b28f333dce77cf9d7f6fa95ef1fcc7cf760dc3c5f12ff9fcf88ce16251ce93&X-Amz-SignedHeaders=host&x-amz-checksum-mode=ENABLED&x-id=GetObject",
+  "S1": {
+    url: "https://storage.railway.app/organized-pierogi-ssnev8/frofudrljocldrugudr295ifistlsw1w.m3u8?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=tid_Wzgsfcv_JTSuyvynvo_ERVPvp_zFPkylviswDZvLuepKWFpcJq%2F20260114%2Fauto%2Fs3%2Faws4_request&X-Amz-Date=20260114T073925Z&X-Amz-Expires=3600&X-Amz-Signature=de0ac20dcb1300f48ae11d0ef223e2e52458d79584c8829b0c505b3921ec73f0&X-Amz-SignedHeaders=host&x-amz-checksum-mode=ENABLED&x-id=GetObject",
     poster: "#"
   },
   "BBL": {
     url: "https://aps33.playlist.live-video.net/v1/playlist/CroF8x5yO3O1N_x1aqXte9URwFEyHWC68U3bxMpNGp7ofnQQqPlM_IyEhGxLdd0VBTFoiRQD6PqrCgBREKEIOhE5g7omcyKX2_Rpr2VKXd4tVqADeQlswFxoLSyUHCobTYx2_vA3WlVCapz97edb8LIXjdcnaedigUgkVC1emAK7Q9sasXr-tt_xM1zr9jicLX6si0HRUduf_VwskbJ9T8Ze6pUjbYYt0Z5EFEpIUjt-E6ZYKS9XQBGr3mIXhTcDndSekIdOLCvQKbAlTKpJjJkse1jUG44JRHT3sNzot8Q1tJa03twcGRdqfuRV5aRMQKaZeidPg_oWW5OexPhsFjDA8suTeiEHkA6MJe-cXfdviRKQku7TQe_mv4ZLFYpvSZXHYnA-zjMsaMzrRkRbYdO604iKKNakrDNGg1Ik0JhKxP52B_llb788PdEkGqSFW3ltcp4xwW7_WUw3MGIJ2ExFLFADMqmxFpR1_G_PiZB6IumHi8CONdzb-RUh7nKplJR_hHrxE5-RlW6WvyiTqsqf7XWbqgmzHzHO_j7vhvPFXyXO1hIim5U9IDJSnwJIDWxHVLk0o3CAAc9K_tLdb2dbHbdvUzE4IFR3mJfc2Ew5-WSvPX83qCXXxB2PiJRTK8AeleocLBxKTAfk2vT2-2_Tcjd9IZ63WuGwueAXqSd2M9YvfoYK8CJ8OL_73Qb1IHE_QTywIQ9RSaNrhgA3IW0Jm4Iainzjt6NGy-vGtYCoDr1K-ZxGqc70E2z1t8DiYDwd716LR92mIssFaG9DgVwzjt2sViS2UeEG1G7EGwLLY-qfy7W0givudV4NOywmjzb2X4OIHitHZ8I3oj6XqKfg8IBYdJGhb-ozk-Gg0OBTBuBGpfDSNoIxT8PiOXLC9UITYHRU3LNX2YgqPOOTGo2EwANLCoLkC8BXD7saDJc8lkjTMUcQcE4DaiABKgl1cy13ZXN0LTIwhA4.m3u8",
     poster: "#"
   },
-  "S1": {
+  "S": {
     url: "https://edge1-moblive.yuppcdn.net/drm/smil:peosports1drm.smil/index.m3u8",
     poster: "#"
   },
