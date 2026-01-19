2ddd8f50b677dec0bd8da92cacb2e8fde7b4b4b4
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2026-01-19 19:35:15 +0530

    Update premium.js

--- premium.js
+++ premium.js
@@ -1,7 +1,7 @@
 // streams.js
 const streamMap = {
-  "PM1": {
-    url: "https://ru001.madayonoska.cam/out/v2/40ed94e5873e2d1e6426cdaf44baeb4d/index.m3u8",
+  "S1": {
+    url: "https://storage.railway.app/organized-pierogi-ssnev8/7e8f9a0b1c2d3e4f5a6b7c8d9e0f1a2b.m3u8?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=tid_Wzgsfcv_JTSuyvynvo_ERVPvp_zFPkylviswDZvLuepKWFpcJq%2F20260119%2Fauto%2Fs3%2Faws4_request&X-Amz-Date=20260119T140431Z&X-Amz-Expires=3600&X-Amz-Signature=1e5a9fda0fdec336577c4c5f420e225ebf2f5976c53f3adeb47bd92e866ddbd3&X-Amz-SignedHeaders=host&x-amz-checksum-mode=ENABLED&x-id=GetObject",
     poster: "#"
   },
   "DD": {
