8eaf2b542055cb30ce807a9fc4cd29a2bd78fff6
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2026-01-24 23:20:56 +0530

    Update adds.js

--- adds.js
+++ adds.js
@@ -1,5 +1,5 @@
 const streams = {
-  "U1": "https://matchify.zip/home/blog2/",
+  "PL1": "https://matchify.zip/home/blog1/",
   "U2": "https://matchify.zip/home/blog1/",
   "U3": "https://matchify.zip/home/blog3/",
   "U4": "https://matchify.zip/home/blog4/",
@@ -7,9 +7,9 @@ const streams = {
   "U6": "https://matchify.zip/home/blog6/",
   "U7": "https://matchify.zip/home/blog7/",
   "B1": "https://matchify.zip/home/blog8/",
-  "L1": "https://matchify.zip/home/blog6/",
+  "L1": "https://matchify.zip/home/blog4/",
   "F1": "https://ballcontrol.click/home/blogf/",
-  "UFC": "https://ballcontrol.click/mma/blog/",
+  "UFC": "https://matchify.zip/mma/blog/",
   "S1": "https://matchify.zip/home/blog7/",
   "CF": "https://hotnewsnation.com/home/blog2/",
   "F1": "https://voodc.com/embed/85818c999d8ca184847a8598a38798868582.html",
