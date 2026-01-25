b4b20769b2307bb65d929b819f7e8ca1468097a7
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2026-01-25 13:52:51 +0530

    Update premium.js

--- premium.js
+++ premium.js
@@ -1,7 +1,7 @@
 // streams.js
 const streamMap = {
   "S1": {
-    url: "https://allinonereborn.xyz/amit/hls2.php",
+    url: "https://cold-surf-39b1.xojiv79335.workers.dev/&i=1",
     poster: "#"
   },
   "S": {
