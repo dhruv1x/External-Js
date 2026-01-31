70ba5e6f2bf3a6a649a6d6e69c52d47e7b0e98d2
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2026-02-01 00:20:05 +0530

    Update premium.js

--- premium.js
+++ premium.js
@@ -24,12 +24,12 @@ const streamMap = {
     url: "https://amg01269-amg01269c1-sportstribal-emea-5204.playouts.now.amagi.tv/playlist/amg01269-willowtvfast-willowplus-sportstribalemea/playlist.m3u8",
     poster: "#"
   },
-    "S4": {
+    "RM": {
     url: "https://tvsen5.aynascope.net/c9EpzZ6fQBJ3/index.m3u8?e=1769178230&u=78be6644-0a65-48ec-81a4-089ac65a2619&token=6089b7e8983c05030b3022b68d697c13",
     poster: ""
   },
-  "BS": {
-    url: "https://ru001.madayonoska.cam/out/v2/4d65fc467f85194944e16a89b121c92d/index.m3u8",
+  "WWE": {
+    url: "https://601.exylcraft.us/wwe.m3u8",
     poster: ""
   },
 
