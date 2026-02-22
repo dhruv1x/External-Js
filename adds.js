7330379965c8c643ed46c0f6fc115c6ddb4de4fd
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2026-02-22 22:07:35 +0530

    Update adds.js

--- adds.js
+++ adds.js
@@ -11,7 +11,7 @@ const streams = {
   "F1": "https://ballcontrol.click/home/blogf/",
   "UFC": "https://gametimes.top/mma/blog/",
   "B1": "https://gametimes.top/home/blog3/",
-  "S1": "https://gametimes.plus/home/blog3/",
+  "S1": "https://gametimes.plus/home/blog5/",
   "LG1": "https://gametimes.plus/home/blog2/",
   
 };
