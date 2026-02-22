6ba752db3b78ca50b221579ef5e6a43867345f85
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2026-02-22 20:45:12 +0530

    Update adds.js

--- adds.js
+++ adds.js
@@ -7,7 +7,7 @@ const streams = {
   "U7": "https://matchify.zip/home/blog8/",
   "U8": "https://matchify.zip/home/blog4/",
   "CB": "https://soco.livecdnem.com/ajax/chanel/type/5/link/channel78/off-tvc",
-  "L1": "https://gametimes.plus/home/blog2/",
+  "L1": "https://gametimes.plus/home/blog4/",
   "F1": "https://ballcontrol.click/home/blogf/",
   "UFC": "https://gametimes.top/mma/blog/",
   "B1": "https://gametimes.top/home/blog3/",
