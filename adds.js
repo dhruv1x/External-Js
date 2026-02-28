b17b06930435023a55c8502e056d93f8dea5009d
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2026-02-28 22:53:20 +0530

    Update adds.js

--- adds.js
+++ adds.js
@@ -1,17 +1,17 @@
 const streams = {
-  "U1": "https://areagame.net/home/blog1/",
+  "PL1": "https://areagame.net/home/blog1/",
   "U2": "https://areagame.net/home/blog2/",
   "U3": "https://areagame.net/home/blog3/",
   "PL1": "https://gametimes.plus/home/blog1/",
   "PL5": "https://gametimes.plus/home/blog5/",
   "U7": "https://matchify.zip/home/blog8/",
   "U8": "https://matchify.zip/home/blog4/",
-  "S1": "https://viewembed.ru/channel/bet12012898",
+  "S1": "https://areagame.net/home/blog2/",
   "L1": "https://areagame.net/home/blog4/",
   "F1": "https://ballcontrol.click/home/blogf/",
   "UFC": "https://gametimes.top/mma/blog/",
   "B1": "https://gametimes.top/home/blog3/",
   "S2": "https://gametimes.plus/home/blog5/",
-  "LG1": "https://gametimes.plus/home/blog2/",
+  "LG1": "https://areagame.net/home/blog3/",
   
 };
