10d3d6761e802436ebac8bb034794da181eef072
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2026-03-01 13:30:33 +0530

    Create configure.js

---
+++ configure.js
@@ -0,0 +1,10 @@
+const DOMAIN_CONFIG = {
+    allowedDomains: [
+        "extraw.pages.dev",
+        "extragc.pages.dev"
+    ]
+};
+
+const TELEGRAM_CONFIG = {
+    popupChannel: "https://telegram.me/+WOHjeld4UV9lYWJl"
+};
