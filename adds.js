28e126496c7654940e319044e1b27c7b27abadcb
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2026-03-04 18:30:42 +0530

    Update adds.js

--- adds.js
+++ adds.js
@@ -5,7 +5,7 @@ const streams = {
   "PL1": "https://gametimes.plus/home/blog1/",
   "PL5": "https://gametimes.plus/home/blog5/",
   "U7": "https://matchify.zip/home/blog8/",
-  "U8": "https://matchify.zip/home/blog4/",
+  "U8": "https://viewembed.ru/channel/SupersportNews%5BSouthAfrica%5D",
   "S1": "https://areagame.net/home/blog2/",
   "L1": "https://areagame.net/home/blog4/",
   "F1": "https://ballcontrol.click/home/blogf/",
