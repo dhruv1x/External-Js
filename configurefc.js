3b9d076d6d58a7fad5e10a29625b4b74e072b386
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2026-03-07 14:58:46 +0530

    Create configurefc.js

---
+++ configurefc.js
@@ -0,0 +1,10 @@
+const DOMAIN_CONFIG = {
+    allowedDomains: [
+        "livecrichdtv.pages.dev",
+        "extrav.pages.dev"
+    ]
+};
+
+const TELEGRAM_CONFIG = {
+    popupChannel: "https://telegram.me/+WOHjeld4UV9lYWJl"
+};
