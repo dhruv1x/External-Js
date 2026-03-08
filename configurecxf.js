faa52728c6cf5bff87ad88b15434757bb5fdad7f
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2026-03-08 08:19:00 +0530

    Create configurecxf.js

---
+++ configurecxf.js
@@ -0,0 +1,10 @@
+const DOMAIN_CONFIG = {
+    allowedDomains: [
+        "extracxf.pages.dev",
+        "extrav.pages.dev"
+    ]
+};
+
+const TELEGRAM_CONFIG = {
+    popupChannel: "https://telegram.me/+L7agYSmJVRNkZDFl"
+};
