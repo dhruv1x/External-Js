5ee671afe30d959d8cf03ca6a89ff542982a944f
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2026-03-15 00:53:50 +0530

    Update premium.js

--- premium.js
+++ premium.js
@@ -72,8 +72,8 @@ const streamMap = {
     url: "https://allinonereborn.store/fcww/live222.php?url=https://islstream.xojiv79335.workers.dev/?id=140462_HINDI",
     poster: ""
   },
-  "V1": {
-    url: "https://allinonereborn.store/fcww/live222.php?url=https://herzogdell.s3.us-east-1.amazonaws.com/mancity.m3u8",
+  "S1": {
+    url: "https://allinonereborn.store/fcww/live222.php?url=https://storage.googleapis.com/unikhsgyrbover3/mux_video_ts1/index-1.m3u8",
     poster: "#"
   },
   "F2": {
