5084f76385d1ea7230044232f9d5e12cb4c6e866
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2026-03-19 01:09:25 +0530

    Update jantv.js

--- jantv.js
+++ jantv.js
@@ -1,10 +1,47 @@
-// streams.js
+// stream.js
 
-export const COOKIE = "__hdnea__=st=1773844261~exp=1773865861~acl=/*~hmac=5a47de28e8c4964b93c1609443db1f34df10d3fce1ecc2e09e6a1d62e283c593";
+window.STREAM_DATA = {
 
-export const sonyLivSports = {
-  'SN1': { name: 'Telegram-cricXfootball 1', streamUrl: 'https://jiotvpllive.cdn.jio.com/bpk-tv/SonyLIV_Sports_1_MOB/WDVLive/index.mpd', clearKey: { keyId: '4e4c5dba5bfe5a7c9338bc086eb80379', key: 'b8e8932f440084e1d2b705fa52eef94b' } },
-  'SN2': { name: 'Telegram-cricXfootball 2', streamUrl: 'https://jiotvpllive.cdn.jio.com/bpk-tv/SonyLIV_Sports_2_MOB/WDVLive/index.mpd', clearKey: { keyId: 'ef24516ae38b50d18049343cd1cd998a', key: 'eb4810ec250a1795a0019dba7881ff45' } },
-  'SN3': { name: 'Telegram-cricXfootball 3', streamUrl: 'https://jiotvpllive.cdn.jio.com/bpk-tv/SonyLIV_Sports_3_MOB/WDVLive/index.mpd', clearKey: { keyId: '3afa887864e6545aa006db21aeb39054', key: '6b8cce271f3cd923d0df32d45b44f6df' } },
-  'SN4': { name: 'Telegram-cricXfootball 5', streamUrl: 'https://jiotvpllive.cdn.jio.com/bpk-tv/SonyLIV_Sports_5_MOB/WDVLive/index.mpd', clearKey: { keyId: 'cee8550bf50b569d8ad2ed0c5264f0d2', key: '565a95f1c332c03534e1e5fcc1ff9a8b' } }
+  COOKIE: "__hdnea__=st=1773844261~exp=1773865861~acl=/*~hmac=5a47de28e8c4964b93c1609443db1f34df10d3fce1ecc2e09e6a1d62e283c593",
+
+  sonyLivSports: {
+
+    'SN1': {
+      name: 'Telegram-cricXfootball 1',
+      streamUrl: 'https://jiotvpllive.cdn.jio.com/bpk-tv/SonyLIV_Sports_1_MOB/WDVLive/index.mpd',
+      clearKey: {
+        keyId: '4e4c5dba5bfe5a7c9338bc086eb80379',
+        key: 'b8e8932f440084e1d2b705fa52eef94b'
+      }
+    },
+
+    'SN2': {
+      name: 'Telegram-cricXfootball 2',
+      streamUrl: 'https://jiotvpllive.cdn.jio.com/bpk-tv/SonyLIV_Sports_2_MOB/WDVLive/index.mpd',
+      clearKey: {
+        keyId: 'ef24516ae38b50d18049343cd1cd998a',
+        key: 'eb4810ec250a1795a0019dba7881ff45'
+      }
+    },
+
+    'SN3': {
+      name: 'Telegram-cricXfootball 3',
+      streamUrl: 'https://jiotvpllive.cdn.jio.com/bpk-tv/SonyLIV_Sports_3_MOB/WDVLive/index.mpd',
+      clearKey: {
+        keyId: '3afa887864e6545aa006db21aeb39054',
+        key: '6b8cce271f3cd923d0df32d45b44f6df'
+      }
+    },
+
+    'SN4': {
+      name: 'Telegram-cricXfootball 5',
+      streamUrl: 'https://jiotvpllive.cdn.jio.com/bpk-tv/SonyLIV_Sports_5_MOB/WDVLive/index.mpd',
+      clearKey: {
+        keyId: 'cee8550bf50b569d8ad2ed0c5264f0d2',
+        key: '565a95f1c332c03534e1e5fcc1ff9a8b'
+      }
+    }
+
+  }
+
 };
