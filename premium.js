632c64ebaa99e9f09ab2ed4869f5c473b6cb1e44
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2026-03-28 01:26:50 +0530

    Update premium.js

--- premium.js
+++ premium.js
@@ -1,7 +1,7 @@
 // streams.js
 const streamMap = {
   "U1": {
-    url: "https://live5.msrktz.app/live/24561735.m3u8",
+    url: "https://imgs-001.juno-api-noe.icu/secure/api/v1/ev-b5dde3373a.m3u8",
     poster: "#"
   },
   "F1": {
