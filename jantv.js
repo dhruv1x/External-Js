e67d2333ce5307709d45ecad27e4a3bfef470a88
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2026-04-05 15:28:51 +0530

    Update jantv.js

--- jantv.js
+++ jantv.js
@@ -8,7 +8,7 @@ window.STREAM_DATA = {
 
     'E1': {
       name: 'Telegram-cricXfootball 1',
-      streamUrl: 'https://jiotvpllive.cdn.jio.com/bpk-tv/Star_Sports_HD1_BTS/output/index.mpd',
+      streamUrl: 'https://jiotvioslive.cdn.jio.com/bpk-tv/Star_Sports_HD1_BTS/output/index.mpd',
       clearKey: {
         keyId: '965dc2ddb1d85138ad787999a7f30ca5',
         key: '859695076e67fe961836b564db6d689c'
@@ -17,7 +17,7 @@ window.STREAM_DATA = {
 
     'H2': {
       name: 'Telegram-cricXfootball 2',
-      streamUrl: 'https://jiotvpllive.cdn.jio.com/bpk-tv/StarSportsHD2Hin_BTS/output/index.mpd',
+      streamUrl: 'https://jiotvioslive.cdn.jio.com/bpk-tv/StarSportsHD2Hin_BTS/output/index.mpd',
       clearKey: {
         keyId: 'bd1ea6a1c1fb5dcdb709434e0826bbc0',
         key: 'f57f22cba32739ce7beb1fab3f3d060b'
@@ -43,7 +43,7 @@ window.STREAM_DATA = {
     },
     'H1': {
       name: 'Telegram-cricXfootball 5',
-      streamUrl: 'https://jiotvpllive.cdn.jio.com/bpk-tv/Star_Sports_HD1_Hindi_BTS/output/index.mpd',
+      streamUrl: 'https://jiotvioslive.cdn.jio.com/bpk-tv/Star_Sports_HD1_Hindi_BTS/output/index.mpd',
       clearKey: {
         keyId: '400131994b445d8c8817202248760fda',
         key: '2d56cb6f07a75b9aff165d534ae2bfc4'
