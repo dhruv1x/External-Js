1da054fa253686a820adf815532cde51f0039a17
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2026-04-10 19:57:42 +0530

    Update premium.js

--- premium.js
+++ premium.js
@@ -5,7 +5,7 @@ const streamMap = {
     poster: "#"
   },
   "S3": {
-    url: "https://ru-001.winter-spring-api.shop/secure/api/v1/lchd-ipltata.m3u8",
+    url: "https://ru-001.winter-spring-api.shop/secure/api/v1/lchd-ipl-hindi.m3u8",
     poster: ""
   },
   "PSL2": {
@@ -85,7 +85,7 @@ const streamMap = {
     poster: "#"
   },
   "S1": {
-    url: "https://ru-001.winter-spring-api.shop/secure/api/v1/lchd-fs501.m3u8",
+    url: "https://ru-001.winter-spring-api.shop/secure/api/v1/lchd-ipl-english.m3u8",
     poster: ""
   },
 };
