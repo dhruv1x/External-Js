76dfde9839b1965fe263195eac7ba7ab73ed9571
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2026-04-12 15:42:29 +0530

    Update premium.js

--- premium.js
+++ premium.js
@@ -5,7 +5,7 @@ const streamMap = {
     poster: "#"
   },
   "S3": {
-    url: "https://ru-001.winter-spring-api.shop/secure/api/v1/lchd-ipl-english.m3u8",
+    url: "",
     poster: ""
   },
   "PSL2": {
@@ -85,7 +85,7 @@ const streamMap = {
     poster: "#"
   },
   "S1": {
-    url: "https://lb-nl-50gbs.zenostreams-cdn-001.com/secure/api/v1/lchd-fs501.m3u8",
+    url: "https://lb-nl-50gbs.zenostreams-cdn-001.com/secure/api/v1/lchd-ipl-english.m3u8",
     poster: ""
   },
 };
