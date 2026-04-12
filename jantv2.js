00e9bab5d8ca056b2b01a2e34b5109b2c3a39e05
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2026-04-12 14:59:20 +0530

    Create jantv2.js

---
+++ jantv2.js
@@ -0,0 +1,19 @@
+// streams.js
+
+window.STREAMS = {
+
+  h1: {
+    mpd: "https://example.com/stream1/index.mpd",
+    keyId: "keyid_here",
+    key: "key_here",
+    cookie: "__hdnea__=your_cookie_here"
+  },
+
+  h2: {
+    mpd: "https://example.com/stream2/index.mpd",
+    keyId: "keyid_here2",
+    key: "key_here2",
+    cookie: "__hdnea__=your_cookie_here2"
+  }
+
+};
