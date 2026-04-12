94e6bdbfe7296f0c198a14d58ab8a83fdcb8f61f
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2026-04-12 15:17:26 +0530

    Update jantv2.js

--- jantv2.js
+++ jantv2.js
@@ -2,14 +2,14 @@
 
 window.STREAMS = {
 
-  h1: {
-    mpd: "https://example.com/stream1/index.mpd",
-    keyId: "keyid_here",
-    key: "key_here",
-    cookie: "__hdnea__=your_cookie_here"
+  E2: {
+    mpd: "https://jiotvpllive.cdn.jio.com/bpk-tv/Star_Sports_HD2_BTS/output/index.mpd?__hdnea__=st=1775976611~exp=1775998211~acl=/bpk-tv/Star_Sports_HD2_BTS/output/*~hmac=3a1621542643658df0187805e9d4b13cc20bd7a01494b05dc483261d0bb7ccab",
+    keyId: "9457eb90129456fa8ea95e10ba4ac51e",
+    key: "e620a970cea474c491ac78ae71a4d764",
+    cookie: "__hdnea__=st=1775976611~exp=1775998211~acl=/bpk-tv/Star_Sports_HD2_BTS/output/*~hmac=3a1621542643658df0187805e9d4b13cc20bd7a01494b05dc483261d0bb7ccab"
   },
 
-  h2: {
+  H2: {
     mpd: "https://jiotvpllive.cdn.jio.com/bpk-tv/StarSportsHD2Hin_BTS/output/index.mpd?__hdnea__=st=1775982753~exp=1776004353~acl=/bpk-tv/StarSportsHD2Hin_BTS/output/*~hmac=16d37e55235cce2780522a19ae5f4ef95bc0737a9127309f75d7d19c7cc4ac46",
     keyId: "bd1ea6a1c1fb5dcdb709434e0826bbc0",
     key: "f57f22cba32739ce7beb1fab3f3d060b",
