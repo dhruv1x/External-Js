987a1e47e898047319579bca862dda2d3dcd5fa2
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2026-04-12 15:51:13 +0530

    Update jantv2.js

--- jantv2.js
+++ jantv2.js
@@ -14,6 +14,20 @@ window.STREAMS = {
     keyId: "bd1ea6a1c1fb5dcdb709434e0826bbc0",
     key: "f57f22cba32739ce7beb1fab3f3d060b",
     cookie: "__hdnea__=st=1775982753~exp=1776004353~acl=/bpk-tv/StarSportsHD2Hin_BTS/output/*~hmac=16d37e55235cce2780522a19ae5f4ef95bc0737a9127309f75d7d19c7cc4ac46"
+  },
+
+  E1: {
+    mpd: "https://jiotvpllive.cdn.jio.com/bpk-tv/Star_Sports_HD1_BTS/WDVLive/index.mpd?__hdnea__=st=1775910617~exp=1775932217~acl=/bpk-tv/Star_Sports_HD1_BTS/WDVLive/*~hmac=5de528c92849588f78260a883ca8847cdb3ee5facc96af4f8cf3423ffde57cb7",
+    keyId: "965dc2ddb1d85138ad787999a7f30ca5",
+    key: "859695076e67fe961836b564db6d689c",
+    cookie: "__hdnea__=st=1775910617~exp=1775932217~acl=/bpk-tv/Star_Sports_HD1_BTS/WDVLive/*~hmac=5de528c92849588f78260a883ca8847cdb3ee5facc96af4f8cf3423ffde57cb7"
+  },
+
+  H1: {
+    mpd: "https://jiotvpllive.cdn.jio.com/bpk-tv/Star_Sports_HD1_Hindi_BTS/WDVLive/index.mpd?__hdnea__=st=1775970112~exp=1775991712~acl=/bpk-tv/Star_Sports_HD1_Hindi_BTS/WDVLive/*~hmac=53d03d9a3b38fc1abf7ceb6ab0ec00f4f3e89c6a2e09a02e6de1a009acc77fc5",
+    keyId: "400131994b445d8c8817202248760fda",
+    key: "2d56cb6f07a75b9aff165d534ae2bfc4",
+    cookie: "__hdnea__=st=1775970112~exp=1775991712~acl=/bpk-tv/Star_Sports_HD1_Hindi_BTS/WDVLive/*~hmac=53d03d9a3b38fc1abf7ceb6ab0ec00f4f3e89c6a2e09a02e6de1a009acc77fc5"
   }
 
 };
