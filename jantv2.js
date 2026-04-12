32b3c66124040d9aa7ed2161946269466bb04373
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2026-04-12 15:00:49 +0530

    Update jantv2.js

--- jantv2.js
+++ jantv2.js
@@ -10,10 +10,10 @@ window.STREAMS = {
   },
 
   h2: {
-    mpd: "https://example.com/stream2/index.mpd",
-    keyId: "keyid_here2",
-    key: "key_here2",
-    cookie: "__hdnea__=your_cookie_here2"
+    mpd: "https://jiotvpllive.cdn.jio.com/bpk-tv/StarSportsHD2Hin_BTS/output/index.mpd?__hdnea__=st=1775982753~exp=1776004353~acl=/bpk-tv/StarSportsHD2Hin_BTS/output/*~hmac=16d37e55235cce2780522a19ae5f4ef95bc0737a9127309f75d7d19c7cc4ac46",
+    keyId: "bd1ea6a1c1fb5dcdb709434e0826bbc0",
+    key: "f57f22cba32739ce7beb1fab3f3d060b",
+    cookie: "__hdnea__=st=1775982753~exp=1776004353~acl=/bpk-tv/StarSportsHD2Hin_BTS/output/*~hmac=16d37e55235cce2780522a19ae5f4ef95bc0737a9127309f75d7d19c7cc4ac46"
   }
 
 };
