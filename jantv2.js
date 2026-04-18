0c6d2dedd695119953e6a86a1f7e88c99fb54c5e
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2026-04-18 19:31:29 +0530

    Update jantv2.js

--- jantv2.js
+++ jantv2.js
@@ -3,17 +3,17 @@
 window.STREAMS = {
 
   E2: {
-    mpd: "https://jiotvpllive.cdn.jio.com/bpk-tv/Star_Sports_HD2_BTS/output/index.mpd?__hdnea__=st=1775976611~exp=1775998211~acl=/bpk-tv/Star_Sports_HD2_BTS/output/*~hmac=3a1621542643658df0187805e9d4b13cc20bd7a01494b05dc483261d0bb7ccab",
+    mpd: "https://jiotvpllive.cdn.jio.com/bpk-tv/Star_Sports_HD2_BTS/WDVLive/index.mpd?__hdnea__=st=1776519013~exp=1776540613~acl=/bpk-tv/Star_Sports_HD2_BTS/WDVLive/*~hmac=30e0710c84d49123d925ec2368fd709eef44714c9cba7139ce0030479906b4bf",
     keyId: "9457eb90129456fa8ea95e10ba4ac51e",
     key: "e620a970cea474c491ac78ae71a4d764",
-    cookie: "__hdnea__=st=1775976611~exp=1775998211~acl=/bpk-tv/Star_Sports_HD2_BTS/output/*~hmac=3a1621542643658df0187805e9d4b13cc20bd7a01494b05dc483261d0bb7ccab"
+    cookie: "__hdnea__=st=1776519013~exp=1776540613~acl=/bpk-tv/Star_Sports_HD2_BTS/WDVLive/*~hmac=30e0710c84d49123d925ec2368fd709eef44714c9cba7139ce0030479906b4bf"
   },
 
   H2: {
-    mpd: "https://jiotvpllive.cdn.jio.com/bpk-tv/StarSportsHD2Hin_BTS/output/index.mpd?__hdnea__=st=1775982753~exp=1776004353~acl=/bpk-tv/StarSportsHD2Hin_BTS/output/*~hmac=16d37e55235cce2780522a19ae5f4ef95bc0737a9127309f75d7d19c7cc4ac46",
+    mpd: "https://jiotvpllive.cdn.jio.com/bpk-tv/StarSportsHD2Hin_BTS/WDVLive/index.mpd?__hdnea__=st=1776519014~exp=1776540614~acl=/bpk-tv/StarSportsHD2Hin_BTS/WDVLive/*~hmac=2485cdb47ed64d85f09e4362f3c852e1363858a19c6d531646e2f2f6803fc7e6",
     keyId: "bd1ea6a1c1fb5dcdb709434e0826bbc0",
     key: "f57f22cba32739ce7beb1fab3f3d060b",
-    cookie: "__hdnea__=st=1775982753~exp=1776004353~acl=/bpk-tv/StarSportsHD2Hin_BTS/output/*~hmac=16d37e55235cce2780522a19ae5f4ef95bc0737a9127309f75d7d19c7cc4ac46"
+    cookie: "__hdnea__=st=1776519014~exp=1776540614~acl=/bpk-tv/StarSportsHD2Hin_BTS/WDVLive/*~hmac=2485cdb47ed64d85f09e4362f3c852e1363858a19c6d531646e2f2f6803fc7e6"
   },
 
   H1: {
