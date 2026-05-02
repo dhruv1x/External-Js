9c465d643eb2c362c95ae79c35adabc47528e1e2
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2026-05-02 19:18:12 +0530

    Update jantv2.js

--- jantv2.js
+++ jantv2.js
@@ -17,17 +17,17 @@ window.STREAMS = {
   },
 
   H1: {
-    mpd: "https://jiotvpllive.cdn.jio.com/bpk-tv/Star_Sports_HD1_Hindi_BTS/WDVLive/index.mpd?__hdnea__=st=1777716294~exp=1777737894~acl=/bpk-tv/Star_Sports_HD1_Hindi_BTS/WDVLive/*~hmac=3fbf0a41f10d7ab1ab16152424baa07edf8a34e8f606b932e92c100d9fd9c586",
+    mpd: "https://jiotvpllive.cdn.jio.com/bpk-tv/Star_Sports_HD1_Hindi_BTS/WDVLive/index.mpd?__hdnea__=st=1777729117~exp=1777750717~acl=/bpk-tv/Star_Sports_HD1_Hindi_BTS/WDVLive/*~hmac=1764abf410c1e437f30e8d659535e3346c6e6c793f5a438da169931a67d46533",
     keyId: "400131994b445d8c8817202248760fda",
     key: "2d56cb6f07a75b9aff165d534ae2bfc4",
-    cookie: "__hdnea__=st=1777716294~exp=1777737894~acl=/bpk-tv/Star_Sports_HD1_Hindi_BTS/WDVLive/*~hmac=3fbf0a41f10d7ab1ab16152424baa07edf8a34e8f606b932e92c100d9fd9c586"
+    cookie: "__hdnea__=st=1777729117~exp=1777750717~acl=/bpk-tv/Star_Sports_HD1_Hindi_BTS/WDVLive/*~hmac=1764abf410c1e437f30e8d659535e3346c6e6c793f5a438da169931a67d46533"
   },
 
   E1: {
-    mpd: "https://jiotvpllive.cdn.jio.com/bpk-tv/Star_Sports_HD1_BTS/WDVLive/index.mpd?__hdnea__=st=1777716294~exp=1777737894~acl=/bpk-tv/Star_Sports_HD1_BTS/WDVLive/*~hmac=4b0fd5c53ac6dcdd3950a14cfc2d37eae3372c7a2c8a6923c26b4d9ddb47ff6b",
+    mpd: "https://jiotvpllive.cdn.jio.com/bpk-tv/Star_Sports_HD1_BTS/WDVLive/index.mpd?__hdnea__=st=1777729116~exp=1777750716~acl=/bpk-tv/Star_Sports_HD1_BTS/WDVLive/*~hmac=f41648a187e3889e31ad82cd82fe917fe600314dfd94aee52af0985f6498ac91",
     keyId: "965dc2ddb1d85138ad787999a7f30ca5",
     key: "859695076e67fe961836b564db6d689c",
-    cookie: "__hdnea__=st=1777716294~exp=1777737894~acl=/bpk-tv/Star_Sports_HD1_BTS/WDVLive/*~hmac=4b0fd5c53ac6dcdd3950a14cfc2d37eae3372c7a2c8a6923c26b4d9ddb47ff6b"
+    cookie: "__hdnea__=st=1777729116~exp=1777750716~acl=/bpk-tv/Star_Sports_HD1_BTS/WDVLive/*~hmac=f41648a187e3889e31ad82cd82fe917fe600314dfd94aee52af0985f6498ac91"
   }
 
 };
