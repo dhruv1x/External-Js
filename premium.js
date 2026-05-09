a76616a71dd71d1a5cf4a2b5a2692c18acb23dc5
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2026-05-09 17:08:18 +0530

    Update premium.js

--- premium.js
+++ premium.js
@@ -45,7 +45,7 @@ const streamMap = {
     poster: ""
   },
   "H": {
-    url: "",
+    url: "https://dai-partner.fancode.com/primary/140510_english_hls_d8d1c0470714697_1ta-di_h264/index.m3u8",
     poster: ""
   },
   "E": {
@@ -57,7 +57,7 @@ const streamMap = {
     poster: ""
   },
   "B": {
-    url: "",
+    url: "https://dai-partner.fancode.com/primary/140510_english_hls_d8d1c0470714697_1ta-di_h264/index.m3u8",
     poster: ""
   },
   "L1": {
