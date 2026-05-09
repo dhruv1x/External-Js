fbd36ad66449a64202fc770d40d594dc53cfddf1
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2026-05-09 22:34:12 +0530

    Create configureextra.js

---
+++ configureextra.js
@@ -0,0 +1,10 @@
+const DOMAIN_CONFIG = {
+    allowedDomains: [
+        "famcodexweb.pages.dev",
+        "famcodexwebs.pages.dev"
+    ]
+};
+
+const TELEGRAM_CONFIG = {
+    popupChannel: "https://telegram.me/+9Ueyh8bfyl9kNmY1"
+};
