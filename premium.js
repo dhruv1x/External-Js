7d7609e86d6b3738d8f33e05e74ae04033221064
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2026-05-16 19:42:32 +0530

    Update premium.js

--- premium.js
+++ premium.js
@@ -5,7 +5,7 @@ const streamMap = {
     poster: "#"
   },
   "S3": {
-    url: "https://sunffov.kuvuslov.cymru/ipl-hindi.m3u8",
+    url: "",
     poster: ""
   },
   "PSL2": {
@@ -65,7 +65,7 @@ const streamMap = {
     poster: ""
   },
   "S4": {
-    url: "https://sunffov.kuvuslov.cymru/ipl-english.m3u8",
+    url: "",
     poster: ""
   },
   "U1": {
