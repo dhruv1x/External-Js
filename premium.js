142c87416f4fff49978c90c63897f4aec32774a5
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2026-05-21 19:25:28 +0530

    Update premium.js

--- premium.js
+++ premium.js
@@ -5,7 +5,7 @@ const streamMap = {
     poster: "#"
   },
   "S3": {
-    url: "",
+    url: "https://nevazak.musiala.rest/ipl-hindi.m3u8",
     poster: ""
   },
   "PSL2": {
@@ -17,55 +17,55 @@ const streamMap = {
     poster: "#"
   },
   "J": {
-    url: "https://eventcdn02-nowe.akamaized.net/hls/CH621/index.m3u8",
+    url: "https://dai-partner.fancode.com/primary/140517_english_hls_40c91fbbcb59479_1ta-di_h264/index.m3u8",
     poster: ""
   },
   "O": {
-    url: "https://eventcdn02-nowe.akamaized.net/hls/CH622/index.m3u8",
+    url: "https://dai-partner.fancode.com/primary/140517_english_hls_40c91fbbcb59479_1ta-di_h264/index.m3u8",
     poster: ""
   },
   "P": {
-    url: "https://eventcdn02-nowe.akamaized.net/hls/CH623/index.m3u8",
+    url: "https://dai-partner.fancode.com/primary/140518_english_hls_9481b78c2323004_1ta-di_h264/index.m3u8",
     poster: ""
   },
   "C": {
-    url: "https://eventcdn02-nowe.akamaized.net/hls/CH624/index.m3u8",
+    url: "https://dai-partner.fancode.com/primary/140518_english_hls_9481b78c2323004_1ta-di_h264/index.m3u8",
     poster: ""
   },
   "S": {
-    url: "https://eventcdn02-nowe.akamaized.net/hls/CH625/index.m3u8",
+    url: "https://dai-partner.fancode.com/primary/140515_english_hls_49fd742e7342225_1ta-di_h264/index.m3u8",
     poster: ""
   },
   "I": {
-    url: "https://eventcdn02-nowe.akamaized.net/hls/CH626/index.m3u8",
+    url: "https://dai-partner.fancode.com/primary/140515_english_hls_49fd742e7342225_1ta-di_h264/index.m3u8",
     poster: ""
   },
   "N": {
-    url: "https://eventcdn02-nowe.akamaized.net/hls/CH627/index.m3u8",
+    url: "https://dai-partner.fancode.com/primary/140515_english_hls_49fd742e7342225_1ta-di_h264/index.m3u8",
     poster: ""
   },
   "R": {
-    url: "https://dai-partner.fancode.com/primary/140520_hindi_hls_926b963c5948932_1ta-di_h264/index.m3u8",
+    url: "https://dai-partner.fancode.com/primary/140515_english_hls_49fd742e7342225_1ta-di_h264/index.m3u8",
     poster: ""
   },
   "E": {
-    url: "https://dai-partner.fancode.com/primary/140520_english_hls_c9b15435ae71949_1ta-di_h264/index.m3u8",
+    url: "https://dai-partner.fancode.com/primary/140519_english_hls_bf04da179766803_1ta-di_h264/index.m3u8",
     poster: ""
   },
   "B": {
-    url: "https://dai-partner.fancode.com/primary/140520_bengali_hls_f495ff50d334746_1ta-di_h264/index.m3u8",
+    url: "https://dai-partner.fancode.com/primary/140519_english_hls_bf04da179766803_1ta-di_h264/index.m3u8",
     poster: ""
   },
   "L": {
-    url: "https://dai-partner.fancode.com/primary/140520_malayalam_hls_e2f1ca91e057747_1ta-di_h264/index.m3u8",
+    url: "https://dai-partner.fancode.com/primary/140519_english_hls_bf04da179766803_1ta-di_h264/index.m3u8",
     poster: ""
   },
   "H": {
-    url: "https://perjaka.s3.us-east-1.amazonaws.com/btsport.m3u8",
+    url: "https://dai-partner.fancode.com/primary/140519_english_hls_bf04da179766803_1ta-di_h264/index.m3u8",
     poster: ""
   },
   "S4": {
-    url: "",
+    url: "https://nevazak.musiala.rest/ipl-english.m3u8",
     poster: ""
   },
   "U1": {
@@ -85,7 +85,7 @@ const streamMap = {
     poster: "#"
   },
   "S1": {
-    url: "",
+    url: "https://nevazak.musiala.rest/fox501.m3u8",
     poster: ""
   },
 };
