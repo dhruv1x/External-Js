4325288441d0ba07005ac624694890755eaee253
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2026-06-13 22:15:48 +0530

    Create configurefifawccxf.js

---
+++ configurefifawccxf.js
@@ -0,0 +1,11 @@
+const DOMAIN_CONFIG = {
+    allowedDomains: [
+        "fifawcbycxf.pages.dev",
+        "estrav.pages.dev",
+        "#.pages.dev"
+    ]
+};
+
+const TELEGRAM_CONFIG = {
+    popupChannel: "https://telegram.me/fifawcbycxf"
+};
