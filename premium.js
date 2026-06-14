74172a3ad4df43ba7d21c655c0cfdcde137f502c
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2026-06-15 00:49:18 +0530

    Update premium.js

--- premium.js
+++ premium.js
@@ -81,7 +81,7 @@ const streamMap = {
     poster: ""
   },
   "U2": {
-    url: "https://ataide0.formaturamaxi.com.br/tsn.m3u8",
+    url: "https://live05.msdht.app/live/24561735.m3u8",
     poster: "#"
   },
   "S1": {
