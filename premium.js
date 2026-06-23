d0a6ccc3c9c55d595f426025ff887c2bcb56ab42
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2026-06-23 21:21:30 +0530

    Update premium.js

--- premium.js
+++ premium.js
@@ -77,7 +77,7 @@ const streamMap = {
     poster: ""
   },
   "U1": {
-    url: "https://live05.apusport.com/live/08552895.m3u8",
+    url: "https://live05.apusport.com/live/87547578.m3u8",
     poster: ""
   },
   "U2": {
