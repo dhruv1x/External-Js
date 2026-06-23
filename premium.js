6f0e2e7f9f0763b3a8d98d35bc31253e9b80fe65
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2026-06-23 22:35:32 +0530

    Update premium.js

--- premium.js
+++ premium.js
@@ -81,7 +81,7 @@ const streamMap = {
     poster: ""
   },
   "U2": {
-    url: "https://doret.s3.us-east-1.amazonaws.com/live/match_1.m3u8",
+    url: "https://wa.newzela.online/tooota1.m3u8",
     poster: "#"
   },
   "S1": {
