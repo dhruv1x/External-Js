095d2365d75346d84a5c275ca5752e406f7c0d04
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2026-06-26 04:33:29 +0530

    Update premium.js

--- premium.js
+++ premium.js
@@ -45,7 +45,7 @@ const streamMap = {
     poster: ""
   },
   "U7": {
-    url: "https://ataide0.mercadodavoz.com.br/tsn.m3u8",
+    url: "https://mazy-flinez.s3.eu-north-1.amazonaws.com/reta/sa1_720p30.m3u8",
     poster: ""
   },
   "NBA": {
