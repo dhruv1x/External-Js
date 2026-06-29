b8721a76b2c7193408b20f7318b8d16f3829567b
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2026-06-30 00:45:24 +0530

    Update premium.js

--- premium.js
+++ premium.js
@@ -53,7 +53,7 @@ const streamMap = {
     poster: ""
   },
   "U1": {
-    url: "https://mickiecy.s3.eu-central-1.amazonaws.com/btsport_1080p30.m3u8",
+    url: "https://mickiecy.s3.eu-central-1.amazonaws.com/sportsnet_1080p30.m3u8",
     poster: ""
   },
   "U4": {
