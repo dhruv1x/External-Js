d51506184c6a2552c646c0a92c68e1fa8f7b646c
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2026-06-30 16:49:31 +0000

    Update premium.js

--- premium.js
+++ premium.js
@@ -53,7 +53,7 @@ const streamMap = {
     poster: ""
   },
   "U5": {
-    url: "https://live05.koepgd.app/live/24561735.m3u8",
+    url: "https://live2.eu-north-1b.cf.dmcdn.net/sec2(QEj2Ay_zPrH8i6UEFTwESJsif5k00cWdIeEUgaREEcnmPCeXZ6s2h8sx0aknwMSPcWNJMCveoaOHuaDi8y7CTVT2i4Y-IFZzQEvXxBRY17HydP0YqCBHdVDwe7_QMT8-)/cloud/3/x9ko9z8/s/live-720.m3u8",
     poster: ""
   },
   "U4": {
@@ -77,7 +77,7 @@ const streamMap = {
     poster: ""
   },
   "U1": {
-    url: "https://live05.koepgd.app/live/24561735.m3u8",
+    url: "https://live2.eu-north-1b.cf.dmcdn.net/sec2(QEj2Ay_zPrH8i6UEFTwESJsif5k00cWdIeEUgaREEcnmPCeXZ6s2h8sx0aknwMSPcWNJMCveoaOHuaDi8y7CTVT2i4Y-IFZzQEvXxBRY17HydP0YqCBHdVDwe7_QMT8-)/cloud/3/x9ko9z8/s/live-720.m3u8",
     poster: ""
   },
   "U2": {
