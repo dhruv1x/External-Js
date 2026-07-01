3683b9586d6d91c42231d666078064d916e5b11b
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2026-07-01 23:11:01 +0530

    Update premium.js

--- premium.js
+++ premium.js
@@ -5,7 +5,7 @@ const streamMap = {
     poster: "#"
   },
   "S3": {
-    url: "https://cdn011.viaplus.site/sky-sports-cricket.m3u8",
+    url: "https://cricbuzz.newpersonalities.workers.dev/https://livevideos.cricbuzz.com/hls/live/2110089/MENA-SEA/10532/129392/master.m3u8",
     poster: ""
   },
   "PSL2": {
