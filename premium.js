307c9a6137eab6df4d0a7f371c1532ae806d10ce
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2026-07-24 21:02:44 +0530

    Update premium.js

--- premium.js
+++ premium.js
@@ -81,7 +81,7 @@ const streamMap = {
     poster: ""
   },
   "U2": {
-    url: "https://foxdtc-video.akamaized.net/ZXhwPTE3ODQ1Njk4MTc7YWNsPS8qO3dzaWQ9ZGMxZjljNTI3YmNiYWM3MTY5ZDA4ZDgyNjBkMDg2ZjNfZm94ZHRjX3dlYl9lbi1VUztobWFjPVBvcFdWWUpTMERKUjNMTEVZOWZDOVFFVDhKbklzSFpBaWRSMnBVaXRtVTA9/live/tx001-ue2/index.m3u8",
+    url: "https://live05.meung.app/live/08552895.m3u8",
     poster: "#"
   },
   "S1": {
