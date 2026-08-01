c2a02eb631142a54137b3cfdb2d19affdbe903c6
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2026-08-01 15:23:57 +0530

    Update akami.js

--- akami.js
+++ akami.js
@@ -3,11 +3,11 @@ const id = params.get("id");
 
 const streams = {
 
-S2: "https://players.akamai.com/players/hlsjs?streamUrl=https://dishmt.slivcdn.com/hls/live/2011747/TEN1HD/hdntl=exp=1785571630~acl=%2f*~id=3b35b572-ee31-4da1-a600-2421b52fdfca~data=hdntl~hmac=96762f7d4fe17b7551c0faaa9fefa4a1d03c000ab7a1138c53ac3d299d59c238/master_3500.m3u8",
-S1: "https://players.akamai.com/players/hlsjs?streamUrl=https://dishmt.slivcdn.com/hls/live/2020434/TEN2HD/hdntl=exp=1785571630~acl=%2f*~id=3b35b572-ee31-4da1-a600-2421b52fdfca~data=hdntl~hmac=96762f7d4fe17b7551c0faaa9fefa4a1d03c000ab7a1138c53ac3d299d59c238/master_3500.m3u8",
-S3: "https://players.akamai.com/players/hlsjs?streamUrl=https://dishmt.slivcdn.com/hls/live/2020591/TEN3HD/hdntl=exp=1785571630~acl=%2f*~id=3b35b572-ee31-4da1-a600-2421b52fdfca~data=hdntl~hmac=96762f7d4fe17b7551c0faaa9fefa4a1d03c000ab7a1138c53ac3d299d59c238/master_3500.m3u8",
-S4: "https://players.akamai.com/players/hlsjs?streamUrl=https://dishmt.slivcdn.com/hls/live/2020589/ten4hd/hdntl=exp=1785571630~acl=%2f*~id=3b35b572-ee31-4da1-a600-2421b52fdfca~data=hdntl~hmac=96762f7d4fe17b7551c0faaa9fefa4a1d03c000ab7a1138c53ac3d299d59c238/master_3500.m3u8",
-S5: "https://players.akamai.com/players/hlsjs?streamUrl=https://dishmt.slivcdn.com/hls/live/2020593/SONYSIXHD/hdntl=exp=1785571630~acl=%2f*~id=3b35b572-ee31-4da1-a600-2421b52fdfca~data=hdntl~hmac=96762f7d4fe17b7551c0faaa9fefa4a1d03c000ab7a1138c53ac3d299d59c238/master_3500.m3u8"
+S1: "https://players.akamai.com/players/hlsjs?streamUrl=https://dishmt.slivcdn.com/hls/live/2011747/TEN1HD/hdntl=exp=1785658035~acl=%2f*~id=f97165a8-5ced-453e-8883-77ff41708e0b~data=hdntl~hmac=67d88dee8a77ef64bfe92c38eea6226771fb8478a377f7e9056ffc1651eeaffa/master_3500.m3u8",
+S2: "https://players.akamai.com/players/hlsjs?streamUrl=https://dishmt.slivcdn.com/hls/live/2020434/TEN2HD/hdntl=exp=1785658035~acl=%2f*~id=f97165a8-5ced-453e-8883-77ff41708e0b~data=hdntl~hmac=67d88dee8a77ef64bfe92c38eea6226771fb8478a377f7e9056ffc1651eeaffa/master_3500.m3u8",
+S3: "https://players.akamai.com/players/hlsjs?streamUrl=https://dishmt.slivcdn.com/hls/live/2020591/TEN3HD/hdntl=exp=1785658035~acl=%2f*~id=f97165a8-5ced-453e-8883-77ff41708e0b~data=hdntl~hmac=67d88dee8a77ef64bfe92c38eea6226771fb8478a377f7e9056ffc1651eeaffa/master_3500.m3u8",
+S4: "https://players.akamai.com/players/hlsjs?streamUrl=https://dishmt.slivcdn.com/hls/live/2020589/ten4hd/hdntl=exp=1785658035~acl=%2f*~id=f97165a8-5ced-453e-8883-77ff41708e0b~data=hdntl~hmac=67d88dee8a77ef64bfe92c38eea6226771fb8478a377f7e9056ffc1651eeaffa/master_3500.m3u8",
+S5: "https://players.akamai.com/players/hlsjs?streamUrl=https://dishmt.slivcdn.com/hls/live/2020593/SONYSIXHD/hdntl=exp=1785658035~acl=%2f*~id=f97165a8-5ced-453e-8883-77ff41708e0b~data=hdntl~hmac=67d88dee8a77ef64bfe92c38eea6226771fb8478a377f7e9056ffc1651eeaffa/master_3500.m3u8"
 
 };
 
