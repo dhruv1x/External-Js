fc3b76b81d1fe54b962d1747f9f9c731df72974c
dhruv1x <147925382+dhruv1x@users.noreply.github.com>
2026-08-23 16:43:46 +0530

    Update akami.js

--- akami.js
+++ akami.js
@@ -4,7 +4,7 @@ const id = params.get("id");
 const streams = {
 
 S1: "https://players.akamai.com/players/hlsjs?streamUrl=https://dishmt.slivcdn.com/hls/live/2011747/TEN1HD/hdntl=exp=1786874841~acl=%2f*~id=2f25697f-7690-409f-a164-7263b9b3997b~data=hdntl~hmac=de679dcd724b58252270deb95d9b3b86e0ca8d7c75b5d9b99d1780869be21a33/master_3500.m3u8",
-S2: "https://players.akamai.com/players/hlsjs?streamUrl=https://dishmt.slivcdn.com/hls/live/2020434/TEN2HD/hdntl=exp=1787293815~acl=%2f*~id=800d35b2-5239-41e4-83f7-e0c115105bf2~data=hdntl~hmac=7d51a0f804ae024d12ff9893e37f3075ad2ed877029afa5380ea6654183d0c0f/master_3500.m3u8",
+S2: "https://players.akamai.com/players/hlsjs?streamUrl=https://dishmt.slivcdn.com/hls/live/2020434/TEN2HD/hdntl=exp=1787553014~acl=%2f*~id=08df7061-305d-455a-8753-ff53553a4fd4~data=hdntl~hmac=62c37a450b5abb20490a7dfa5751c8300390ec3a2449253be2e95f5d3ce5f29d/master_3500.m3u8",
 S3: "https://players.akamai.com/players/hlsjs?streamUrl=https://dishmt.slivcdn.com/hls/live/2020591/TEN3HD/hdntl=exp=1786874841~acl=%2f*~id=2f25697f-7690-409f-a164-7263b9b3997b~data=hdntl~hmac=de679dcd724b58252270deb95d9b3b86e0ca8d7c75b5d9b99d1780869be21a33/master_3500.m3u8",
 S4: "https://players.akamai.com/players/hlsjs?streamUrl=https://dishmt.slivcdn.com/hls/live/2020589/ten4hd/hdntl=exp=1785744438~acl=%2f*~id=0ef6a40b-2643-4d97-9b32-b94abda1ba73~data=hdntl~hmac=746fb0336b935b0476a3e6198b88834716800e1ee11c9a9c4222f0addf17966d/master_3500.m3u8",
 S5: "https://players.akamai.com/players/hlsjs?streamUrl=https://dishmt.slivcdn.com/hls/live/2020593/SONYSIXHD/hdntl=exp=1785744438~acl=%2f*~id=0ef6a40b-2643-4d97-9b32-b94abda1ba73~data=hdntl~hmac=746fb0336b935b0476a3e6198b88834716800e1ee11c9a9c4222f0addf17966d/master_3500.m3u8"
